4bff5c03042c151e8ba70a9e4aded3e3b2dfc94e
Giao Ho <joutvhu@gmail.com>
2021-04-03 14:23:17 +0700

    Change log output format

--- src/io-helper.ts
+++ src/io-helper.ts
@@ -57,40 +57,16 @@ export function getInputs(): ReleaseInputs {
     return result;
 }
 
-export function setOutputs(outputs: any, log?: boolean) {
+export function setOutputs(response: any, log?: boolean) {
     // Get the outputs for the created release from the response
-    const {
-        id,
-        node_id,
-        url,
-        html_url,
-        upload_url,
-        assets_url,
-        name,
-        tag_name,
-        body,
-        draft,
-        prerelease,
-        target_commitish,
-        created_at,
-        published_at
-    } = outputs;
+    let message = '';
+    for (const key in Outputs) {
+        const field: string = (Outputs as any)[key];
+        if (log)
+            message += `\n  ${field}: ${JSON.stringify(response[field])}`;
+        core.setOutput(Outputs.Id, response[field]);
+    }
 
     if (log)
-        core.info(JSON.stringify(outputs));
-
-    core.setOutput(Outputs.Id, id.toString());
-    core.setOutput(Outputs.NodeId, node_id);
-    core.setOutput(Outputs.Url, url);
-    core.setOutput(Outputs.HtmlUrl, html_url);
-    core.setOutput(Outputs.UploadUrl, upload_url);
-    core.setOutput(Outputs.AssetsUrl, assets_url);
-    core.setOutput(Outputs.TagName, tag_name);
-    core.setOutput(Outputs.Name, name);
-    core.setOutput(Outputs.Body, body);
-    core.setOutput(Outputs.Draft, draft);
-    core.setOutput(Outputs.PreRelease, prerelease);
-    core.setOutput(Outputs.TargetCommitish, target_commitish);
-    core.setOutput(Outputs.PreRelease, created_at);
-    core.setOutput(Outputs.PreRelease, published_at);
+        core.info('Outputs:' + message);
 }
